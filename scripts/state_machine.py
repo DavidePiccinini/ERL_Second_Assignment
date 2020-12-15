5e4b8484bf4a0ef67019e446e0e8cf2a887b759e
Davide Piccinini <piccio98dp@gmail.com>
2020-12-15 20:46:26 +0100

    Developed the play behaviour

--- scripts/state_machine.py
+++ scripts/state_machine.py
@@ -13,16 +13,24 @@ import random
 import actionlib
 import cv2
 import imutils
+import math
 import numpy as np
 from scipy.ndimage import filters
 from actionlib_msgs.msg import GoalStatus
 from geometry_msgs.msg import PoseStamped, Twist
-from std_msgs.msg import String
+from std_msgs.msg import String, Float64
 from sensor_msgs.msg import CompressedImage
 import erl_second_assignment.msg
 
 ## Action client
 actC = None
+
+## Publishers
+velPub = None
+headJointPub = None
+
+## Subscriber
+imageSub = None
 
 ## Goal pose
 pos = PoseStamped()
@@ -32,6 +40,9 @@ sleepCounter = 0
 
 ## Flag to notify that the robot has seen the ball
 ballFound = False
+
+## Flag to notify that the robot has stopped
+robotStopped = False
 
 ##
 # 
@@ -63,8 +74,48 @@ def checkForBall(ros_data):
 # 
 def trackBall(ros_data):
     global ballFound
-    ballFound = False
-    #
+    global robotStopped
+    global velPub
+    
+    np_arr = np.fromstring(ros_data.data, np.uint8)
+    image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)  # OpenCV >= 3.0:
+
+    greenLower = (50, 50, 20)
+    greenUpper = (70, 255, 255)
+
+    blurred = cv2.GaussianBlur(image_np, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    if len(cnts) > 0:
+        # The robot has seen the ball
+        ballFound = True
+
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        if radius > 10:
+            vel = Twist()
+            vel.angular.z = 0.002*(center[0]-400)
+            vel.linear.x = 0.01*(radius-100)
+
+            if vel.linear.x == 0 and vel.angular.z == 0:
+                # The robot has stopped
+                robotStopped = True
+
+            velPub.publish(vel)
+
+    else:
+        # Set the flag to False
+        ballFound = False
+
 
 ##
 # Define Normal state
@@ -82,6 +133,7 @@ class Normal(smach.State):
         global sleepCounter
         global ballFound
         global actC
+        global imageSub
         global pos
 
         imageSub = rospy.Subscriber("robot/camera1/image_raw/compressed", CompressedImage, checkForBall,  queue_size=1)
@@ -115,8 +167,6 @@ class Normal(smach.State):
                     # Go into the PLAY state
                     print('NORMAL state: The robot saw the ball and wants to play.\n')
                     return 'play'
-
-            #     #time.sleep(2)
 
             # Increment the counter
             sleepCounter += 1
@@ -163,7 +213,7 @@ class Sleep(smach.State):
 
         # Go back to the NORMAL state
         sleepCounter = 0
-        print('SLEEP state: The robot woke up.\n')
+        print("SLEEP state: The robot woke up.\n")
         return 'wakeup'
 
 ##
@@ -173,32 +223,63 @@ class Play(smach.State):
         smach.State.__init__(self, outcomes=['stopplaying'])
 
         # Threshold
-        # self.timeThreshold = random.randint(30, 40)
-        self.timeThreshold = 5
+        # self.idleThreshold = random.randint(30, 40)
+        # self.idleThreshold = 5
 
     def execute(self, userdata):
         print('State machine: Executing state PLAY.\n')
 
         global sleepCounter
         global ballFound
-        # global actC
+        global imageSub
+        global headJointPub
 
-        imageSub = rospy.Subscriber("robot/camera1/image_raw/compressed", CompressedImage, trackBall,  queue_size=1)
-
-        timePassed = 0
+        imageSub = rospy.Subscriber("robot/camera1/image_raw/compressed", CompressedImage, trackBall, queue_size=1)
+        headJointPub = rospy.Publisher("robot/joint1_position_controller/command", Float64, queue_size=1)
 
         # Keep incrementing the time counter if the robot doesn't see the ball
-        while timePassed < self.timeThreshold:
+        while not rospy.is_shutdown():
             # Check if the velocity of the robot is 0: if so, move the head
+            if robotStopped == True:
+                imageSub.unregister()
+
+                angle = Float64()
+                angle.data = 0.0
+
+                while angle > -(math.pi/4):
+                    angle.data = angle.data - 0.1
+                    headJointPub.publish(angle)
+                    time.sleep(0.001)
+                time.sleep(2)
+
+                while angle < (math.pi/4):
+                    angle.data = angle.data + 0.1
+                    headJointPub.publish(angle)
+                    time.sleep(0.001)
+                time.sleep(2)
+
+                while angle > 0:
+                    angle.data = angle.data - 0.1
+                    headJointPub.publish(angle)
+                    time.sleep(0.001)
+                time.sleep(2)
+
+                robotStopped = False
+                imageSub = rospy.Subscriber("robot/camera1/image_raw/compressed", CompressedImage, trackBall, queue_size=1)
 
             if ballFound == False:
-                time.sleep(1)
-                timePassed += 1
+                # time.sleep(1)
+                # idleTime += 1
+                startingTime = time.time()
+            while ballFound == False:
+                if (time.time() - startingTime) >= 5:
+                    # Go back to the NORMAL state
+                    imageSub.unregister()
+
+                    print("PLAY state: The robot hasn't seen the ball for a while, so it stops playing.\n")
+                    return 'stopplaying'
         
-        # Go back to the NORMAL state
-        imageSub.unregister()
-        print("PLAY state: The robot hasn't seen the ball for a while, so it stops playing.\n")
-        return 'stopplaying'
+        
 
 ##
 # State machine initialization
